8491ee05322b7ea1d4e22ded5163b87c5d47fb62
jinsb1995 <jinsb1995@gmail.com>
2021-10-08 00:28:57 +0900

    messageCodeResolver, DefaultMessageCodeResolver

--- src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -1,8 +1,13 @@
 package hello.itemservice.validation;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class MessageCodesResolverTest {
 
@@ -12,9 +17,25 @@ public class MessageCodesResolverTest {
     @Test
     void messageCodesResolverObject() {
         String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
         for (String messageCode : messageCodes) {
             System.out.println("messageCode = " + messageCode);
         }
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+//         bindingResult.rejectValue가 내부적으로 resolveMessageCodes를 돌려서 messageCodes[]를 얻고
+//         그걸 할 때 우리가 itemName을 넣어주고, errorCode를 required를 넣어준 것이다.
+//         bindingResult.rejectValue("itemName", "required")
+//         new FieldError("item", "itemName", null, false, messageCodes, null, null);
 
     }
 
